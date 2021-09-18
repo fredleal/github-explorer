94cb104eeda178af65b3218e524df725d331e0c4
Fred Leal <flm_@msn.com>
2021-09-18 10:34:36 -0300

    Consulta a API do github e exibição dos repos

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,4 +1,8 @@
+import { useState, useEffect } from "react"
 import { RepositoryItem } from "./RepositoryItem"
+
+import '../styles/repositories.scss'
+
 
 const repository = {
     name: 'unform',
@@ -7,13 +11,22 @@ const repository = {
 }
 
 export function RepositoryList() {
+
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(()=> {
+        fetch('https://api.github.com/users/fredleal/repos')
+        .then(response => response.json())
+        .then(data => setRepositories(data))
+    }, [repository]);
+
     return (
         <section className="repository-list">
             <h1> Lista de repositórios</h1>
             <ul>
-                <RepositoryItem  repository = {repository}/>
-                <RepositoryItem  repository = {repository}/>
-                <RepositoryItem  repository = {repository}/>
+                {repositories.map(repository =>{
+                    return <RepositoryItem  key={repository.name} repository = {repository}/>
+                })}
             </ul>
         </section>
     )
